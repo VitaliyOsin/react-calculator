de56f17ca03e9b73b6ade769031b4e8c9d47f52e
VitaliyOsin <d_dva@list.ru>
2021-09-09 15:58:18 +0700

    Updates

--- src/components/input/input.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./input.css";
-
-const Input = ({ input }) => {
-  return <div className="input">{input}</div>;
-};
-
-export default Input;
--- src/utils/lists.js
+++
@@ -1,6 +0,0 @@
-export const opers = [
-  { name: "+", value: "+" },
-  { name: "-", value: "-" },
-  { name: "×", value: "*" },
-  { name: "÷", value: "/" },
-];
